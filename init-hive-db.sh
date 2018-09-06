258c9b1360c3b070e6238bf08655af73b709438e
Michael Hess <mhess@connectify.me>
2018-09-06 10:18:38 -0400

    Fixed grant script generation

--- init-hive-db.sh
+++ init-hive-db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+psql -t -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
   CREATE USER hive WITH PASSWORD 'hive';
   CREATE DATABASE metastore;
   GRANT ALL PRIVILEGES ON DATABASE metastore TO hive;
